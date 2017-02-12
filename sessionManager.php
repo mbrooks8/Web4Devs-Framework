2e90bcd654ec8878dbf062495fa481f48907fc42
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-02-12 07:27:01 -0800

    added login

--- sessionManager.php
+++ sessionManager.php
@@ -3,9 +3,15 @@ session_start();
 ?>
 <?php
 /*echo "Favorite color is " . $_SESSION["username"] . ".<br>";*/
-$_SESSION["username"] = $_POST["username"];
-echo "<script>alert('hello')</script>";
-echo "<script>alert('".$_SESSION["username"]."')</script>";
-echo $_SESSION["username"];
-header('Location: ./drone.php');
+$users = array("eperez58","mbrooks8","areyes43","nvaishampayan","dhalili");
+if(in_array($_POST["username"],$users)){
+    $_SESSION["username"] = $_POST["username"];
+    echo "<script>alert('hello')</script>";
+    echo "<script>alert('".$_SESSION["username"]."')</script>";
+    echo $_SESSION["username"];
+    header('Location: ./drone.php');
+}else
+{
+     header('Location: ./login.php');
+}
 ?>
