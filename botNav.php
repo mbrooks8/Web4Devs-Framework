1a5f7822bce2bef524a5fa7c9a5780f2c321c42c
Nicholas Vaishampayan <Nicholas Vaishampayan>
2017-02-12 00:26:38 -0800

    I made the chip

--- botNav.php
+++ botNav.php
@@ -4,17 +4,22 @@
 <footer class="page-footer">
 	<div class="container">
 		<div class="row">
-			<div class="col l6 s12">
+			<div class="col l8 s12">
 				<h5 class="white-text">4Dev</h5>
 				<p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 			</div>
-			<div class="col l4 offset-l2 s12">
+			<div class="col l4 s12">
 				<h5 class="white-text">Links</h5>
 				<ul>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+					<li><div class="chip" "cursor:none;"> <img src = "img/Michael.jpg" alt="Contact Person"><a style="color:black;" href="https://www.linkedin.com/in/michael-brooks-26799693/">Michael Brooks</a></div></li>
+
+					<li><div class="chip" "cursor:none;"> <img src = "img/Deo.jpg" alt="Contact Person"><a style="color:black;" href="https://www.linkedin.com/in/deohalili/">Deo Halili</a></div></li>
+
+					<li><div class="chip" "cursor:none;"> <img src = "img/Esteban.jpg" alt="Contact Person"><a style="color:black;" href="https://www.linkedin.com/in/estebanmagallonperez/">Esteban Perez</a></div></li>
+
+					<li><div class="chip" "cursor:none;"> <img src = "img/Andres.jpg" alt="Contact Person"><a style="color:black;" href="http://r.ddmcdn.com/s_f/o_1/cx_633/cy_0/cw_1725/ch_1725/w_720/APL/uploads/2014/11/too-cute-doggone-it-video-playlist.jpg">Andres Reyes</a></div></li>
+
+					<li><div class="chip" "cursor:none;"> <img src = "img/sloth.jpg" alt="Contact Person"><a style="color:black;" href="https://www.linkedin.com/in/nicholas-vaishampayan-bb8ba2a5/">Nick Vaishampayan</a></div></li>
 				</ul>
 			</div>
 		</div>
@@ -22,7 +27,7 @@
 	<div class="footer-copyright">
 		<div class="container">
 			© 2014 Copyright 4Dev All Rights Reserved
-			<a class="grey-text text-lighten-4 right" href="#!">Please Hire Us</a>
+			<a class="grey-text text-lighten-4 right" href="https://www.linkedin.com/in/estebanmagallonperez/">Please Hire Us</a>
 		</div>
 	</div>
 </footer>
--- drone.php
+++ drone.php
@@ -42,7 +42,7 @@ $user = true;
 				<div class="col s8">
 					<h4 class="white-text">Hire Us<br><span class="secondary">We Are Poor</span></h4>
 				</div>
-				<div class="button raised">Please</div>
+				<div class="button raised"><a style="color:white;" href="https://www.linkedin.com/in/michael-brooks-26799693/">Please</a></div>
 			</div>
 		</div>
 
@@ -85,19 +85,6 @@ $user = true;
 					<li>Board camera for FPV</li>
 
 				</ul>
-<<<<<<< HEAD
-
-			</nav>
-
-			<div class="banner valign">
-				<div class="container">
-					<p><span class="secondary">Revolutionizing</span> Sports and Technology</p>
-					<h1>4Dev<span style="color:white; word-spacing:0;"> • </span>Drone</h1>
-					<p>Bringing <span class="secondary">Skill and Precision</span> to The<span class="secondary"> Next Level</span></p>
-				</div>
-				<a class="button floating"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a>
-=======
->>>>>>> origin/master
 			</div>
 			<p>You'll want to buy a lot of spare props. It also doesn't hurt to have spare batteries, motors and ESCs if you want to avoid waiting around for replacement parts after a crash.
 			</p>
@@ -134,7 +121,7 @@ $user = true;
 		<h5 class="primary center">
 			Your Bank Balance is Going to Suffer</h5>
 		<br>
-		<p>If you like building things, then taking the DIY approach to your first drone is great. For some drone racers, building the quad is half the fun.  In addition to being to have your custom drone look exactly as you want, the other advantage of building your own quad is that you'll have the knowledge and skills to fix it when you crash. The parts pictured below are available at https://www.foreverfpv.com/.
+		<p>If you like building things, then taking the DIY approach to your first drone is great. For some drone racers, building the quad is half the fun.  In addition to being to have your custom drone look exactly as you want, the other advantage of building your own quad is that you'll have the knowledge and skills to fix it when you crash. The parts pictured below are available at <a href="https://www.foreverfpv.com/"> ForeverFPV</a>.
 
 			<!-- end slipsum code --></p>
 		<br>
@@ -150,20 +137,24 @@ $user = true;
 			</div>
 
 			<div class="col s12 m4 center">
-				<div class="imgcard">
-					<img src="http://gettbs.in/image/cache/catalog/motor/LUM7-500x500.jpg
-							  " class="responsive-img">
-					<p>Lumenier RX2206-11 2350Kv
-					</p>
-				</div>
+				<a href="https://www.foreverfpv.com/product/lumenier-rx2206-11-2350kv-motor/">
+					<div class="imgcard">
+						<img src="http://gettbs.in/image/cache/catalog/motor/LUM7-500x500.jpg
+								  " class="responsive-img">
+						<p>Lumenier RX2206-11 2350Kv
+						</p>
+					</div>
+				</a>
 			</div>
 			<div class="col s12 m4 center">
-				<div class="imgcard">
-					<img src="https://images-na.ssl-images-amazon.com/images/I/91WrbKsb3UL._SL1500_.jpg
-							  ">
-					<p>RaceKraft 5051 TCS
-					</p>
-				</div>
+				<a href="https://www.foreverfpv.com/product/racekraft-5051-tcs-tri-blade/">
+					<div class="imgcard">
+						<img src="https://images-na.ssl-images-amazon.com/images/I/91WrbKsb3UL._SL1500_.jpg
+								  ">
+						<p>RaceKraft 5051 TCS
+						</p>
+					</div>
+				</a>
 			</div>
 		</div>
 
@@ -181,7 +172,7 @@ $user = true;
 			</div>
 
 			<div class="col s12 m8 center">
-				<a href="https://www.foreverfpv.com/product/kiss-32bit-flight-controller-v1-06/">
+				<a href="https://www.foreverfpv.com/product/kiss-24a-race-edition-esc/">
 					<div class="imgcard">
 						<img src="https://img.rcgroups.com/http://uploads.tapatalk-cdn.com/20160403/c9f696a78dedead76459a0be609231f6.jpg?h=3IKwvJVSLAWQD0OXh-Vrgw
 								  ">
@@ -208,10 +199,10 @@ $user = true;
 		<h5 class="primary center">
 			Your Bank Balance is Going to Suffer</h5>
 		<br>
-		<p>After some practicing a bit, a great way to improve your skills, meet fellow fpv racers and even trade parts attending local events. Holding events out of Bakersfield California is Bakersfield Multirotor, a <span><a>Tier 1 Chapter of MultiGP MultiRotor Chapter</a></span>.
+		<p>After some practicing a bit, a great way to improve your skills, meet fellow FPV racers and even trade parts attending local events. Holding events out of Bakersfield California is Bakersfield Multirotor, a <span><a>Tier 1 Chapter of MultiGP MultiRotor Chapter</a></span>.
 		</p>
 		<br>
-		<p>Participate in online discussion of fpv news, ask questions or just chit chat on fpv focused communities. Online fpv retailer Forever FPV, has their own online forum in addition to their store. Other FPV online communities are include the FPV Drone Quadcopters Facebook page. For FPV racing events all over the United States follow FPV Racing Events.
+		<p>Participate in online discussion of FPV news, ask questions or just chit chat on FPV focused communities. Online FPV retailer ForeverFPV, has their own online forum in addition to their store. Other FPV online communities are include the FPV Drone Quadcopters Facebook page. For FPV racing events all over the United States follow FPV Racing Events.
 		</p>
 	</div>
 </div>
