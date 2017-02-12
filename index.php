45c15ed0d95b8440a10b34bb99216a5cfbf443f1
Nicholas Vaishampayan <Nicholas Vaishampayan>
2017-02-11 16:18:50 -0800

    Tooltip transition

--- index.php
+++ index.php
@@ -135,7 +135,7 @@ $user = true;
 			<div class="container">
 				<div class="section">
 					<h4 class="center">• CORE Features •</h4>
-					<p class="primary center"><!-- start slipsum code -->
+					<p class="primary center hoverable"><!-- start slipsum code -->
 
 Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.
 
