4633b8b37dd8c2716e4f6e7536b8f2e6cbd0e97b
Afropenguin <mbrooks8@ucmerced.edu>
2017-03-12 01:26:35 -0800

    added google logout button

--- index.php
+++ index.php
@@ -42,6 +42,15 @@
 					<li><a href="#table">Table</a></li>
 					<li><a href="#template">Template</a></li>
 					<li><a href="#utility">Utility</a></li>
+					<a href="#" onclick="signOut();">Sign out</a>
+					<script>
+						function signOut() {
+							var auth2 = gapi.auth2.getAuthInstance();
+							auth2.signOut().then(function () {
+								console.log('User signed out.');
+							});
+						}
+					</script>
 				</ul>
 			</div>
 		</div>
