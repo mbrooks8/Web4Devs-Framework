567eec3e542beb4e90aa40583a60bd2f90a3edf4
Afropenguin <mbrooks8@ucmerced.edu>
2017-03-12 03:44:38 -0700

    logout fixed

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 							var auth2 = gapi.auth2.getAuthInstance();
 							auth2.signOut().then(function () {
 								console.log('User signed out.');
-								location.reload();
+								window.location = "./php/logout.php";
 							});
 						}
 
